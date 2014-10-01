23177f237b67ea0b97acb9e1bba0bb0ed0d09884
roshan <roshan.prakash.r@gmail.com>
2014-10-01 16:05:42 +0100

    Initial Commit

---
+++ SafariTNG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariTNG
+//
+//  Created by roshan on 01/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
